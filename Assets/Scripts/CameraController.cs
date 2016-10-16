c0d0d5356fa7522286351cc15d55cb26d85e208e
Ali Soliman <ali.soliman95@gmail.com>
2016-10-16 17:30:51 +0200

    Change Colors of the ground and add new material

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,7 +12,7 @@ public class CameraController : MonoBehaviour {
 	    offset = transform.position - player.transform.position;
 	}
 
-    
+
 	void LateUpdate () {
 	    transform.position = player.transform.position + offset;
 	}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,30 @@ public class PlayerController : MonoBehaviour {
 
 
     public float speed;
+    private Vector3 movementVertical;
     private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 	    rb = GetComponent<Rigidbody>();
+        movementVertical = new Vector3(0,0,2);
 	}
 
 	void FixedUpdate() {
+
+        //Horizontal Movement between Lanes
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-
+        Vector3 movement = new Vector3(moveHorizontal, 0, 0);
         rb.AddForce(movement * speed);
+
+
+        //Automatic Vertical Movement
+        rb.AddForce(movementVertical);
 	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pick Up")){
+            other.gameObject.SetActive(false);
+        }
+    }
 }
